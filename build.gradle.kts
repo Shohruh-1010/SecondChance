6779090be3fdc831ea896449e5754b1ca683ed25
Shohruh-1010 <shoxruhxusainov2@gmail.com>
2026-05-23 10:39:47 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("fabric-loom") version "1.6.13"
-    id("maven-publish")
-    kotlin("jvm") version "1.9.23"
 }
 
 version = "1.0.0"
@@ -10,24 +8,20 @@ group = "com.secondchance"
 repositories {
     mavenCentral()
     maven("https://maven.fabricmc.net/")
-    maven("https://maven.architectury.dev/")
 }
 
 dependencies {
     minecraft("com.mojang:minecraft:1.21.1")
-    mappings(loom.layered {
-        officialMojangMappings()
-    })
+    mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.16.9")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.108.0+1.21.1")
 }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(21)
-}
-
 java {
-    withSourcesJar()
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.jar {
+    from("LICENSE")
 }
